1c061148735e39d97bf0329fc5e5bbd9a33aa07b
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2021-05-03 15:18:38 +0200

    add one meme comments section

--- src/app/components/memy/mem/mem.component.ts
+++ src/app/components/memy/mem/mem.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpService} from '../../../services/http.service';
 import {IMem} from '../../../models/IMem';
 import {ActivatedRoute} from '@angular/router';
+import {UploadMemeComment} from '../../../models/uploadModels/UploadMemeComment';
 
 @Component({
   selector: 'app-mem',
@@ -14,6 +15,9 @@ export class MemComponent implements OnInit, OnDestroy {
   memId: any;
   private sub: any;
   pathToDir = 'assets/memeImages/';
+  displaySend = false;
+  memeCommentContent: any;
+  memeCommentAuthor: any;
 
   constructor(private route: ActivatedRoute, private httpService: HttpService) { }
 
@@ -32,6 +36,7 @@ export class MemComponent implements OnInit, OnDestroy {
     this.httpService.getMem(this.memId).subscribe(data => {
       console.log(data);
       this.mem = data;
+      this.getMemeComments();
     }, error => {
       console.log(error);
     });
@@ -39,6 +44,27 @@ export class MemComponent implements OnInit, OnDestroy {
 
   makeArrayOfNumbers(iterations: string){
     return Array(iterations.valueOf());
+  }
+
+  allFieldsNotEmpty() {
+    return ((this.memeCommentAuthor != '' && this.memeCommentAuthor != null) && (this.memeCommentContent != '' && this.memeCommentContent != null));
+  }
+
+  submitComment(mem: IMem) {
+    let commentToSubmit: UploadMemeComment = new UploadMemeComment(this.memeCommentContent, this.memeCommentAuthor, mem.id);
+    this.httpService.submitMemeComment(commentToSubmit);
+    this.displaySend = true;
+  }
+
+  private getMemeComments() {
+    this.httpService.getMemeComments(this.mem.id).subscribe(data => {
+      this.mem.commentsList = data;
+      this.mem.commentsList.sort((comment1, comment2) =>
+        (comment1.createdDate > comment2.createdDate) ? 1 : ((comment2.createdDate > comment1.createdDate) ? -1 : 0)
+      );
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
