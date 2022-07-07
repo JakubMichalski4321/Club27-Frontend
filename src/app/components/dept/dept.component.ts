939be5e773378f21d7f6f1470b9199588a25c1c5
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2022-07-07 21:13:44 +0200

    add dept login required

--- src/app/components/dept/dept.component.ts
+++ src/app/components/dept/dept.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dept',
   templateUrl: './dept.component.html',
-  styleUrls: ['./dept.component.css']
+  styleUrls: ['./dept.component.css'],
 })
 export class DeptComponent implements OnInit {
+  showLogin = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.delay(2700);
   }
 
+  private delay(ms: number) {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        this.showLogin = true;
+      }, ms)
+    );
+  }
 }
