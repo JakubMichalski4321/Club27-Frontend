de93d3b6f403a13a91bbaba92552f7111e69a9c5
Jakub Michalski <65559531+JakubMichalski4321@users.noreply.github.com>
2022-11-08 22:41:36 +0100

    impr-bearer-token-service

---
+++ src/app/services/user/bearer-token.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+import { DeptService } from '../comp/dept.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BearerTokenService {
+
+  constructor(
+    private deptService: DeptService,
+  ) { }
+
+  private STORAGE_TOKEN_NAME: string = 'klub27User';
+
+  private token: string = null;
+
+  getToken(): string {
+    if (this.token) {
+      return this.token;
+    } else if (this.getTokenFromStorage()) {
+      return this.getTokenFromStorage().token;
+    };
+  }
+
+  saveToken(token: string) {
+    this.token = token;
+    this.saveTokenInStorage(
+      JSON.stringify({ token: token, username: jwt_decode(token)['sub'] })
+    );
+  }
+
+  logOut(): void {
+    localStorage.removeItem(this.STORAGE_TOKEN_NAME);
+  }
+
+  isTokenAfterExpired(token: string): boolean {
+    if (!token) {
+      return true;
+    } else if (new Date() > new Date(jwt_decode(this.getToken())['exp'] * 1000)) {
+      return true;
+    }
+    return false;
+  }
+
+  getUserId(): string {
+    let fromStorage = this.getTokenFromStorage();
+    if (fromStorage && fromStorage.userId) {
+      return fromStorage.userId;
+    } else if (this.getUserNameFromToken()) {
+      this.deptService.getUserIdByUserName(this.getUserNameFromToken()).subscribe(resp => {
+        fromStorage.userId = resp.value;
+        this.saveTokenInStorage(fromStorage);
+        return resp.value;
+      });
+    }
+  }
+
+  getUserNameFromToken(): string {
+    const token = this.getTokenFromStorage();
+    if (token && token.username) {
+      return token.username;
+    }
+    try {
+      return jwt_decode(this.getToken())['sub'];
+    } catch (Error) {
+      return null;
+    }
+  }
+
+  private getTokenFromStorage(): any {
+    return JSON.parse(localStorage.getItem(this.STORAGE_TOKEN_NAME));
+  }
+
+  private saveTokenInStorage(tokenString: string): void {
+    localStorage.setItem(this.STORAGE_TOKEN_NAME, JSON.stringify(tokenString));
+  }
+
+}
